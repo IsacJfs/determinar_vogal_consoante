a7c281a4f1cfe84d524d1bdcd5d11e8052abcf9c
Isac Desktop <isac.jfs@gmail.com>
2023-06-23 09:51:07 -0300

    Adicionado docstring, implementado try-except, return modificado str -> tuple

--- vogal_consoante.py
+++ vogal_consoante.py
@@ -1,21 +1,40 @@
-print("Identificador de vogal e consoante!")
+print("\nIdentificador de vogal e consoante!\n")
 
 
-def e_letra(letra):
-    # Função criada para determinar se um caractere digitado é ou não uma lentra.
-    if len(letra) > 1:
-        # Conta a quantidade de caracteres digitados, caso maior do que um, informa que foi incorreto
-        # N]ao foi utilizado raise, para que o programa não finalize.
-        return "Erro, digite apenas uma"
-    if letra.isalpha():
-        # isalpha confere se o caractere é uma letra
-        if letra in ['a', 'e', 'i', 'o', 'u']:
-            # Confere se a letra é uma vogal, else: é uma consoante
-            return f"Você digitou a vogal '{letra}'."
+def e_letra(letra: str) -> tuple:
+    """
+    Determina se é uma letra e sendo se é uma vogal ou consoante.
+    :arg:
+        letra (str): Qualquer letra do alfabeto.
+    :returns
+        tuple: retorna uma tupla contendo a letra digitada e o tipo.
+    :raise
+        ValueError: quantidade de caracteres maior que um.
+        TypeError: caractere digitado não é uma letra
+    Exemplo:
+    #   >>> e_letra('a')
+         ('a', 'vogal')
+    """
+    tipo = ""
+    try:
+        if len(letra) > 1:
+            # Conta a quantidade de caracteres digitados, caso maior do que um, informa que foi incorreto
+            raise ValueError
+        if letra.isalpha():
+            # isalpha confere se o caractere é uma letra
+            if letra in ['a', 'e', 'i', 'o', 'u']:
+                # Confere se a letra é uma vogal, else: consoante
+                tipo += "vogal"
+            else:
+                tipo += "consoante"
         else:
-            return f"Você digitou a consoante '{letra}'."
+            raise TypeError
+    except ValueError:
+        print("Digite apenas um caractere.")
+    except TypeError:
+        print("O caractere digitado não é uma letra.")
     else:
-        return f"O caractere digitado '{letra}' não é uma letra."
+        return letra, tipo
 
 
 print(e_letra(input("digite uma letra: ")))  # Coleta o caractere que vai ser analisado.
